2ac0986dcf2a541e338324cf9e284efc09a6991b
alfandialamshach <fandixpc1@gmail.com>
2023-06-23 09:34:18 +0700

    Membuat void remove

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-class Queus {
+class Queues {
 	int FRONT, REAR, max = 5;
 	int queue_array[5];
 
@@ -22,5 +22,24 @@ public:
 			cout << "\nQueue overflow\n";
 			return;
 		}
+
+		//Cek apakah antrian kosong
+		if (FRONT == -1) {
+			FRONT = 0;
+			REAR = 0;
+		}
+		else {
+			// Jika rear berada disisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
+	}
+
+	void remove() {
+		//Apakah antrian kosong
+		in
 	}
 };
