726b8efba1d1a6ae12855147cee7593f83123cb8
alfandialamshach <fandixpc1@gmail.com>
2023-06-23 11:08:30 +0700

    iterasi dari awal array hingga rear

--- CircularQueues/CircularQueues.cpp
+++ CircularQueues/CircularQueues.cpp
@@ -83,8 +83,18 @@ public:
 		else {
 			//Jika FRONT_position > REAR_position, iterasi dari FRON hingga akhir array
 			while (FRONT_position <= max - 1) {
-
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
 			}
+
+			FRONT_position = 0;
+
+			//iterasi dari awal array hingga rear
+			while (FRONT_position <= REAR_position) {
+				cout << queue_array[FRONT_position] << "  ";
+				FRONT_position++;
+			}
+			cout << endl;
 		}
 	}
 	
